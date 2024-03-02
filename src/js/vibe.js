38fca9fb77d70de30326ce7f60b018de1d1920ae
iCeeqSolutions <155289437+iceeqsolutions@users.noreply.github.com>
2024-03-02 19:30:15 +0200

    Modify dynamic text on main page

--- src/js/vibe.js
+++ src/js/vibe.js
@@ -1,8 +1,8 @@
 const vibes = [
-    "...and you are awesome!",
-    "...have a wonderful day!",
-    "...and you've got this!",
-    "...and so is this puppy! 🐶"
+    "...because it was awesome!",
+    "...yeah, me neither!",
+    "...there are no news, which is good news!",
+    "...there will be more news later!"
 ];
 
 let vibe = vibes[Math.floor(Math.random() * Math.floor(vibes.length))];
