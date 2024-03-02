9e532ca14889f6e60f0c5f9f3cff26388f903dc4
iCeeqSolutions <155289437+iceeqsolutions@users.noreply.github.com>
2024-03-02 11:59:20 +0200

    Initial commit

---
+++ .eleventy.js
@@ -0,0 +1,9 @@
+module.exports = function(config) {
+    config.addPassthroughCopy("src/js"); // We add all the content from the folder
+    return {
+        dir: {
+            input: "src",
+            output: "dist"
+        }
+    };
+};
