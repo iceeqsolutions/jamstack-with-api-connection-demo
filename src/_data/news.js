aa4affd41ce9d71e1671dedb1691b77d8f45d9f7
iCeeqSolutions <155289437+iceeqsolutions@users.noreply.github.com>
2024-03-03 15:17:47 +0200

    Add env -file + remove API key from url

--- src/_data/news.js
+++ src/_data/news.js
@@ -1,8 +1,9 @@
+require('dotenv').config();
 const axios = require("axios");
 
 module.exports = async function getData() {
     try {
-        const response = await axios.get('https://newsapi.org/v2/top-headlines?country=se&category=technology&apiKey=33b6f16eb05a4f0482b47f29dd466cfc');
+        const response = await axios.get(`https://newsapi.org/v2/top-headlines?country=se&category=technology&apiKey=${process.env.API_KEY}`);
         return response.data;
     } catch (error) {
         console.log(error);
